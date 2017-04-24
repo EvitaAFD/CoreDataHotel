7c9e1296227ad6489c6a1b7b389e519f828ac43a
EVE DENISON <evedenison@gmail.com>
2017-04-24 09:29:54 -0700

    Initial Commit

---
+++ CoreDataHotel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataHotel
+//
+//  Created by Eve Denison on 4/24/17.
+//  Copyright © 2017 Eve Denison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
