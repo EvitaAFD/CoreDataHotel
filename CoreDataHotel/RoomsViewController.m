817f94fb5bbb531aa629f90af0e5aeea583527a7
EVE DENISON <evedenison@gmail.com>
2017-04-24 19:05:03 -0700

    worked on room view controller delegate and protocols

--- CoreDataHotel/RoomsViewController.m
+++ CoreDataHotel/RoomsViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "RoomsViewController.h"
+#import "AutoLayout.h"
+#import "AppDelegate.h"
+#import "Room+CoreDataClass.h"
+#import "Room+CoreDataProperties.h"
 
-@interface RoomsViewController ()
+@interface RoomsViewController () <UITableViewDataSource>
+
+@property (strong, nonatomic) UITableView *roomsTableView;
+@property (strong, nonatomic) NSArray *allRooms;
 
 @end
 
@@ -16,7 +23,61 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.roomsTableView.backgroundColor = [UIColor whiteColor];
+    self.roomsTableView = [[UITableView alloc]init];
+    
+    self.roomsTableView.dataSource = self;
+    
+    [self.view addSubview:self.roomsTableView];
+    
+    [self.roomsTableView registerClass:[UITableViewCell class]forCellReuseIdentifier:@"roomCell"];
+    
+    self.roomsTableView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [AutoLayout fullScreenConstraintsWithVFLForView:self.roomsTableView];
+}
+
+
+-(NSArray *)allRooms {
+    if (!_allRooms) {
+        
+        AppDelegate *appDelegate = (AppDelegate *)[[UIApplication sharedApplication]delegate];
+        
+        NSManagedObjectContext *context = appDelegate.persistentContainer.viewContext;
+        
+        NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Room"];
+        
+        
+        NSError *fetchError;
+        NSArray *rooms = [context executeFetchRequest:request error:&fetchError];
+        
+        if (fetchError) {
+            NSLog(@"There was an error fatching rooms from Core Data!");
+        }
+        
+        _allRooms = rooms;
+    }
+    
+    return _allRooms;
+    
+}
+
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.allRooms.count;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *roomCell = [tableView dequeueReusableCellWithIdentifier:@"roomCell"];
+    if (roomCell == nil) {
+        roomCell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"roomCell"];
+    }
+    
+    Room *room = self.allRooms[indexPath.row];
+    
+    roomCell.textLabel.text = [NSString stringWithFormat:@"%d", room.number];
+    
+    return roomCell;
+    
+    
 }
 
 
